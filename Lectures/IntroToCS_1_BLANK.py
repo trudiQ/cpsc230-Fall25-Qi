90843da639b0643e99c831196cc86b1ed70bfe40
trudiQ <agivenchy@hotmail.com>
2025-08-22 14:26:25 -0700

    added in-class exercise for lecture 1

---
+++ Lectures/IntroToCS_1_BLANK.py
@@ -0,0 +1,25 @@
+## NOTE Use # to add a comment or remove a line of code from running
+#  Uncomment a line of code by remoing # in front of it
+
+
+# print output, e.g., Welcome
+print("Welcome")
+
+#TODO:
+# Write code to get user input and store it in a variable namded classname
+#   then output (print) "This class is [classname]"
+print("What is this class?")
+# write your code below:
+
+
+
+# SyntaxError
+
+
+# TypeError
+
+
+# NameError
+
+
+# ValueError
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CPSC230-Fall24-Qi
+# CPSC230-Fall25-Qi
 ## Python Programming Enivornment
 - [Python 3.x](https://www.python.org/downloads/)
 - [VS Code](https://code.visualstudio.com/download)
