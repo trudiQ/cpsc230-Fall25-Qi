a7a0506dc7439a0b456517a3c8bf47b915048f7d
Trudi Qi <agivenchy@hotmail.com>
2025-08-29 12:16:39 -0700

    added solution to in_class exercise for lecture 1.

---
+++ Lectures/IntroToCS_1.py
@@ -0,0 +1,31 @@
+## NOTE Use # to add a comment or remove a line of code from running
+#  Uncomment a line of code by remoing # in front of it
+
+
+# print output, e.g., Welcome
+print("Welcome")
+
+#TODO:
+# Write code to get user input and store it in a variable namded classname
+#   then output (print) "This class is [classname]"
+#print("What is this class?")
+# write your code below:
+classname = input("What is this class? ")
+print("This class is ", classname)
+
+
+# SyntaxError
+# uncomment the line below and run to see the error message
+#print('welcome")
+
+# TypeError
+# uncomment the line below and run to see the error message
+#"ABC" + 4 
+
+# NameError
+# uncomment the line below and run to see the error message
+#X = Y
+
+# ValueError
+# uncomment the line below and run to see the error message
+#int("January")
