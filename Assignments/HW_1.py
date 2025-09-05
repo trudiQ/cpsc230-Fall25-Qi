db4a2245a18cfff89f9575d2fde7de2fe5735f4f
Trudi Qi <agivenchy@hotmail.com>
2025-09-05 08:13:10 -0700

    added HW_1

---
+++ Assignments/HW_1.py
@@ -0,0 +1,34 @@
+# Homework for Lectures 1 & 2
+# Remember to save the file before uploading it. 
+
+'''
+1. Write code to print out: Hello, what is your name?
+    then ask the user to input their name, 
+    assign their name to a variable called user_name
+    and print out: Hello [their name]
+'''
+
+
+
+'''
+2. Given variables x and y, can yo write code to assign 9 to x and x + 1 to y, 
+    and print out the values of x and y with one statement? Include comments to EACH line
+    to explain the code.
+'''
+
+
+
+'''
+3. Write a statement that assigns total_coins with the sum of nickel_count and 
+    dime_count to complete the code. Include comments to EACH line
+    to explain the code.
+'''
+total_coins = 0
+nickel_count = int(input())
+dime_count = int(input())
+
+# write your code below
+
+
+# write your code above this line
+print(total_coins)
