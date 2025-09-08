ba79bf61083308546eacd5fd129da96e24672244
Trudi Qi <agivenchy@hotmail.com>
2025-09-08 08:28:38 -0700

    added in-class exercise for lecture 3.

---
+++ Lectures/PythonBasics_3_BLANK.py
@@ -0,0 +1,71 @@
+# Class Activities (Lecture 3)
+
+# Numbers
+'''
+Create a variable x and assign a random integer (e.g., 5) to it and 
+print it and its type in one statement
+'''
+
+
+
+'''
+Create a variable y and assign a random float (e.g., 1.3) to it and 
+print it and its type in one statement
+'''
+
+
+
+# strings
+'''
+Create a string consisting of all types of characters:
+numbers, letters, symbols, and spaces.
+Print out the string and its type.
+'''
+
+
+
+# List
+'''
+Create a list of 1, 2, 3, 4, 5
+'''
+
+
+
+'''
+Create a list consisting of a string, an integer, a float
+and a list.
+Print out the list and its type.
+'''
+
+
+
+# Dictionary
+'''
+Create a variable of dictionary, named cpsc230
+- keys are 'Name', 'School', 'Credits'
+- values are 'Computer Science I', 'Engineer', an integer of 3
+- Print out the dictionary and its type.
+'''
+
+
+
+
+# Set
+'''
+Create a set called my_set, consisting of 1, 2, 3, 4, 5, 5, 5, 6 
+Print out my_set. Did it change at all? 
+Explain why.
+'''
+
+
+
+# Type conversions
+'''
+Create two variables, a and b, assigned with '1.3' and '5', respectively
+Convert the first variable into a float 
+Convert the second variable into an integer
+Sum them up and assign the result to another variable, c
+Print out the value of c, and its type.
+Explain why the result is that type.
+'''
+
