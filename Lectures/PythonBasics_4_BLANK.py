22d5e655c58eb41de2ef55cfad6bc9d01542fef5
Trudi Qi <agivenchy@hotmail.com>
2025-09-10 08:44:21 -0700

    added in-class exercise for lecture 4

---
+++ Lectures/PythonBasics_4_BLANK.py
@@ -0,0 +1,24 @@
+# Class Activities (Lecture 4)
+
+'''
+Without using Python, figure out the answer to this statement.
+
+    10 + 2 ** 3 - (4+5) + 49/7 * 2
+
+1. What's the Answer? BONUS: will this be a float or int?
+2. What Order should we go in?
+3. Rewrite this expression using more parenthese to make it
+    clearer on what order things are run in.
+4. Check your answers using Python.
+'''
+# your answers here
+
+
+
+
+'''
+Using Comparison operators and arithmetic operators, make a complicated 
+expression (use at least 5 operators, repeats allowed) that evaluates to False.
+Check your answer by printing out the result of the expression (should be a False).
+'''
+# your answer here
