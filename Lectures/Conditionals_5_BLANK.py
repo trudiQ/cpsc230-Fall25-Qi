4b1860d636ac075364d4f7c1c9e6b1159aa0ce86
Trudi Qi <agivenchy@hotmail.com>
2025-09-14 11:59:44 -0700

    added in-class exercise for lecture 5.

---
+++ Lectures/Conditionals_5_BLANK.py
@@ -0,0 +1,43 @@
+# Class Activities (Lecture 5)
+
+'''
+Ask the user to input which class they are here for. Store it in the varable class_name.
+Use an if-else statement to check whether class_name is "cpsc230". 
+If is, print "You are in the right classroom!".
+Otherwise, print "You are in the wrong classroom!".
+Use if-else statement.
+'''
+# write your code below
+
+
+
+
+'''
+Ask the user to input an integer and store it in the variable x.
+Convert the value of x into an integer. 
+Then check whether the number is even.
+If yes, print "It's even" Otherwise, print "It's not even."
+Use if-else statement.
+Hint: an even number can be exactly divided by 2, leaving a remainder of 0. 
+'''
+# write your code below
+
+
+
+
+'''
+Let's implement a simpler version of the game "Rock, Paper, Scissors" using conditionals.
+* Rock crushes Scissors
+* Scissors cuts Paper
+* Paper covers Rock
+1. Get two inputs from the user and stored them in variables player1_choice and player2_choice
+2. Compare their choices:
+    Assume player 1 always chooses 'Rock'
+        i.e., assign 'Rock' to player1_choice 
+    When players 1 and 2 choose the same: print "It's a tie!"
+    If player 2 chooses Scissors, print "Player 1 wins! Rock crushes Scissors!"
+    If player 2 chooses Paper, print "Player 2 wins! Paper covers Rock!"
+Use if-else and nested if-else statements. 
+'''
+# write your code below
+
