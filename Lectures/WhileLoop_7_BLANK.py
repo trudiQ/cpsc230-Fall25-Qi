c70ce0f36a41c3741e3ebc2a02b80c724b6b59f8
Trudi Qi <agivenchy@hotmail.com>
2025-09-24 08:16:24 -0700

    added in-class exercise for lecture 7

---
+++ Lectures/WhileLoop_7_BLANK.py
@@ -0,0 +1,61 @@
+# Class Activities (Lecture 7)
+
+'''
+0. What's wrong with this while loop and how can you fix it?
+'''
+# Uncomment the code below and fix it
+#x = 11
+#while x > 10:
+#     print(x)
+
+
+'''
+1. Use a while loop, print out all the numbers between 1 and 10 (inclusive).
+'''
+
+
+
+
+'''
+2. Use a while loop, iterate all the numbers from 1 to 10 (inclusive) 
+and print all the numbers that are greater than 3 and less than or equal to 6.
+Hint: You must use both comparision operators and logical operators.
+'''
+
+
+
+
+'''
+3. Use a while loop, print out all the numbers between 1 and 100 (inclusive) that are
+divisible by BOTH 5 and 9.
+Hint: When a is divisible by b, the remainder of a/b is 0.
+Recall: How to check an even number? (it is a number that is divisible by 2).
+'''
+
+
+
+
+    
+
+'''
+4. Use a while loop, print out all the numbers between 1 and 100 (inclusive) that are
+divisible by both 5 and 9.
+Exit the loop when the number is greater than 50.
+You must use break for this question.
+'''
+
+
+
+
+
+
+
+'''
+5. Use a while loop, print out all the numbers between 1 and 10 (inclusive) 
+EXCEPT for those which are divisiable by 3.
+You must use continue for this question.
+'''
+
+
+
+
