b94f23d1ef5d9f3b7d70a7cf907b65d1e2934e68
Trudi Qi <agivenchy@hotmail.com>
2025-09-29 08:31:01 -0700

    added in-class exercise for lecture 8

---
+++ Lectures/Strings_8_BLANK.py
@@ -0,0 +1,46 @@
+# Class Activities (Lecture 8)
+
+
+'''
+1. Create a string 'Hello World!' and store it to a variable named str1.
+    a. Print out charater 'H' in this string using indexing.
+    b. Slice this string to get 'ello' and assign it to another variable named str2 and print it.
+'''
+
+
+
+
+'''
+2. Use the membership operator in to check if 'Hello World!' contains a space, i.e., ' '
+    if yes, print out "Yes, there is a space."; otherwise "No space."
+'''
+
+
+
+
+'''
+3. Ask the user to input two words and store them into two variables, x and y.
+Use addition (+) to add these two variables together and assign the result back to x.
+print out x in this format: "Here is the combined word: [result]" where result is the value of x.
+'''
+
+
+
+'''
+4. Write a while loop to ask the user to input one character (a letter, a number, or a symbol) at a time.
+Print whatever they just typed. If they typed nothing and hit enter (they created an empty string), 
+print "Nothing was typed. Exit now. " and end the loop using break.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
