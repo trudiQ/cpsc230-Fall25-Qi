d64503239d0d9f4a87e268d37bed0fb04637f692
Trudi Qi <agivenchy@hotmail.com>
2025-10-10 12:57:28 -0700

    added HW6

---
+++ Assignments/HW_6.py
@@ -0,0 +1,28 @@
+# Homework for Lecture 10
+
+'''
+1. Use a for loop and range() function to iterate all the integers from 1 to 10.
+    Check each number if even or odd and print out:
+    "[number] is even" or "[number] is odd"
+'''
+print("QUESTION 1: ")
+# write your code below
+
+
+
+'''
+2. Ask the user to type a sentence made of a few words. Using a for loop to count the number of 
+a's, e's, i's, o's, and u's in that sentence (counting upper and lower cases as the same letter). 
+Print out a message letting the user know the count for each letter. 
+For example, if the user typed "Amazing, thank you!", the message could be: 
+"Your sentence has 3 a, 0 e, 1 i, 1 o, and 1 u"
+Hint: 
+- Use a for loop to iterate all the character in the string (user input sentence)
+        and compare it with a, e, i, o, u, individually, for both upper and lower cases.
+        If the same, increment their counter variables individually,
+            you should have 5 counter variables.
+- Refer to Lecture 10's in-class exercise: Q4.
+'''
+print("QUESTION 2:")
+# write your code below
+
