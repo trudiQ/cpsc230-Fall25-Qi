4ea2f4e44a13cf2b1556bdda0f5b8891d4ad0b93
Trudi Qi <agivenchy@hotmail.com>
2025-10-17 15:48:20 -0700

    added HW7

---
+++ Assignments/HW_7.py
@@ -0,0 +1,51 @@
+# Homework for Lecture 12
+import random
+
+'''
+1. Write some code to check whether the user's input number is an even number or not. Details are below:
+
+PART 1: Define a function, named is_even, with only one input parameter, input_num, check the input parameter to see it's an even number or not, 
+    return True if it is and False otherwise.
+PART 2: Ask the user to input an integer that is greater than 0, call the function you defined above and use the user input integer as the argument.
+    Based on the function's output (True or False), print message "It's an even number" or "It's an odd number" accordingly.
+
+Hint:
+- You may first convert the user typed input into an integer and 
+    then use that integer as an argument when calling the function.
+- The function should have a return statement.
+'''
+# write your code below
+
+
+
+
+
+'''
+2. Write some code to ask the user to guess a random number between 1 and 10. Details are below:
+
+PART 1: Define a function, named guess_the_number, with only one input parameter, input_num. 
+    Inside the function, first, ask the user to type a number between 1 and 10 (inclusive), 
+        and then compare it with the input parameter, input_num. 
+    If they guess it too high, print "It's too high, try again!"; if they guess it too low, print "It's too low, try again!". 
+    Continue asking them until they guess it right and print "You are right!"
+
+PART 2: Use randint(a, b) to randomly generate an integer between 1 and 10 (inclusive) and call the function defined above with
+    that random integer as its argument to see what message the function outputs.
+
+Hint: 
+- randint(a, b) is a function from a built-in library named random (see above import random)
+    that can be used to generate a random integer, N, between two integers, a and b, where a <= N <= b.
+    Refer to Q0 in in-class exercise for Lecture 12 for more details about how to use random.randint(a, b).
+- Use a while loop with True condition inside the function definition for repeatedly asking for user input.
+- The function DOES NOT have a return statement.
+'''
+# write your code below
+
+
+
+
+
+
+
+
+
