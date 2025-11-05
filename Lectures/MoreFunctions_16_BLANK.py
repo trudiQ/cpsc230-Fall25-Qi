9d6c451cc03dce86565bada9ba584db25be86348
Trudi Qi <agivenchy@hotmail.com>
2025-11-05 08:35:01 -0800

    added in-class exercise for lecture 16.

---
+++ Lectures/MoreFunctions_16_BLANK.py
@@ -0,0 +1,60 @@
+# Class Activities (Lecture 16)
+
+'''
+1. Simulate a simple banking system with functions to deposit, withdraw, and check balance.
+    a) Create a global variable account_balance initialized to 0.
+    b) Write a function, deposit, with one input parameter, amount, 
+        that adds the input amount to account_balance; no return.
+    c) Write a function, withdraw, with no input parameter and 
+        only subtracts $10 from account_balance each time; no return.
+    e) Write a function, check_balance, with no input parameter 
+        and only prints the current account_balance; no return.
+    f) Perform a series of deposits and withdrawals, 
+        and check the balance as below. Print out the result.
+        Deposite $100 => Withdraw $10 => Withdraw $10 => Deposite $30 => Check_balance
+NOTE: Define all the required functions first and then call them one after another based on f).
+'''
+print("QUESTION 1: ")
+# write your code below
+
+
+
+
+
+
+'''
+2. Write a function, top_3_movies(), that asks the user to input their top 3 favorite movies at once,
+separated by ','. Split the string into a list of the 3 movie titles (sub-strings).
+Continue asking the user if they DO NOT provide exactly three movies titles.
+Return those three movies titles.
+
+Call the function top_3_movies(), and store those 3 movie titles using 3 variables, 
+mov1, mov2, and mov3. Print out those 3 movie titles. 
+
+Hint: Use string method split() for this question.
+'''
+print("QUESTION 2: ")
+# write your code below
+
+
+
+
+
+
+
+'''
+3. You are developing a hotel rating system where customers can give ratings out of 10.
+The system calculates the average rating, total number of ratings, and a recommendation status.
+    a) Write a function add_hotel_rating(ratings, new_rating) where ratings is a list of existing ratings 
+        and new_rating is the new rating given by a new costomer. The function should:
+        - add the new rating to the list, rating,
+        - calculate the following: 
+            total number of ratings, 
+            the averaged ratings, and
+            a recommendation status: "Recommended" if the average rating is above 7, otherwise "Not Recommended".
+        - return the above three values
+    b) Call the function with a sample list of ratings [7, 8.1] and a new rating, 6
+        and store the returned values in 3 separate variables: average_rating, total_rating_number, recommendation.
+        Print out the updated hotel rating details with these THREE variables.
+'''
+# write your code below
